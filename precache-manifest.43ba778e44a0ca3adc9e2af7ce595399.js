cfa167004222442099e08a73b2a1e26e30d21338
hakeemullah <h.jan210@gmail.com>
2019-04-20 00:54:29 +0500

    Updates

---
+++ precache-manifest.43ba778e44a0ca3adc9e2af7ce595399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4bbd4cc65473aff00d33",
+    "url": "/react-deploy/static/js/runtime~main.82fe89cb.js"
+  },
+  {
+    "revision": "97a14f15960209c2dbdf",
+    "url": "/react-deploy/static/js/main.53ce9580.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-deploy/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "97a14f15960209c2dbdf",
+    "url": "/react-deploy/static/css/main.39ed960c.chunk.css"
+  },
+  {
+    "revision": "f80241e56c4e61d9bd30a9da96b658ea",
+    "url": "/react-deploy/index.html"
+  }
+];
